4b2053de6aa5d6ea785bd428470aa02e801d6180
Yash124gamer <y325dde@gmail.com>
2023-07-27 19:07:16 +0530

    Connected the SMS authentication part to the Sign Up page

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -5,20 +5,34 @@ const accountSid = 'ACca4798b9d2878175188d54ab0a0ed06d';
 const authToken = 'f6bccbfbbbc00486e7bbb214d51bf0ca';
 const client = require('twilio')(accountSid, authToken);
 
+var code = '';
+function generateRandomCode() {
+    code = '';
+    const digits = '0123456789';
+  
+    for (let i = 0; i < 4; i++) {
+      const randomIndex = Math.floor(Math.random() * digits.length);
+      code += digits[randomIndex];
+    }
+  
+    return code;
+  }
+
 router.get('/sendSMS',(req,res)=>{
+    const code = generateRandomCode()
     client.messages
     .create({
-        body: '7098',
+        body: code,
         from: '+12186826963',
         to: '+919870732739'
     })
     .then(message => console.log(message.sid))
-    .then(res.send("Done"))
+    .then(res.send(code))
 
 })
 
 router.get('/checkSMS/',(req,res)=>{
-    if(req.query.otp==="7098")
+    if(req.query.otp===code)
         res.send({
             authentication:"true"
         })
--- src/components/Signuppage.js
+++ src/components/Signuppage.js
@@ -4,21 +4,61 @@ import Input from './Input';
 export default function Loginpage() {
 
     const [name, setName] = useState("");
+    const [pass, setPass] = useState("");
+    const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [number, setNumber] = useState("");
+    const [otp, setOtp] = useState("");
+    const [sms,setSms] = useState(false);
+
+    const sendMsg = async()=>{
+          const url = `http://192.168.246.229:8080/bmt/auth/sendSMS`;
+          const response = await fetch(url);
+          setSms(true)
+        }
+    const checkSMS = async()=>{
+      const url = `http://192.168.246.229:8080/bmt/auth/checkSMS/?otp=${otp}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      if(data.authentication==="true"){
+        document.querySelector('.verify').classList.add('d-none')
+        document.querySelector('.continue').classList.remove('d-none')
+      }
+
+    }
 
   return (
-    <div className="lightDark ">
-      <form className="my-5">
-        <Input label={"Name"} value={name} set={setName} type={"text"} length={"20"}/>
-        <Input label={"Email"} value={email} set={setEmail} type={"email"}/>
-        <Input label={"Number"} value={number} set={setNumber} type={"number"} length={"10"}/>
-        <div className="row justify-content-evenly">
-          <button type="button" class="btn btn-primary w-25 ">
-            Sign In
-          </button>
+    sms ? (
+            <div className="lightDark ">
+            <form className="my-5">
+              <Input label={"OTP-"} value={otp} set={setOtp} type={"number"} length={"4"}/>
+              <div className="row justify-content-evenly">
+                <button type="button" className="btn btn-primary w-25" onClick={checkSMS}>
+                  Verify
+                </button>
+              </div>
+            </form>
+          </div>
+    ) : 
+    (
+          <div className="lightDark ">
+          <form className="my-5">
+            <Input label={"Name"} value={name} set={setName} type={"text"} length={"20"}/>
+            <Input label={"Email"} value={email} set={setEmail} type={"email"}/>
+            <Input label={"Number"} value={number} set={setNumber} type={"number"} length={"10"}/>
+            <Input label={"Username"} value={username} set={setUsername} type={"text"} length={"15"}/>
+            <Input label={"Password"} value={pass} set={setPass} type={"password"} length={"15"}/>
+            <div className="row justify-content-evenly">
+              <button type="button" className=" verify btn btn-primary w-25" onClick={sendMsg}>
+                Sign Up
+              </button>
+              <button type="button" className="continue btn btn-primary w-25 d-none" >
+                Continue
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
+    )
   );
 }
