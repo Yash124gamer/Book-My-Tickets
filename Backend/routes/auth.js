2b5811cadec38452b491a96296ae96be24b68a00
Yash124gamer <y325dde@gmail.com>
2023-07-27 17:51:16 +0530

    Added the SMS authentications API

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const accountSid = 'ACca4798b9d2878175188d54ab0a0ed06d';
 const authToken = 'f6bccbfbbbc00486e7bbb214d51bf0ca';
 const client = require('twilio')(accountSid, authToken);
-var OTP = null;
 
 router.get('/sendSMS',(req,res)=>{
     client.messages
@@ -16,6 +15,17 @@ router.get('/sendSMS',(req,res)=>{
     .then(message => console.log(message.sid))
     .then(res.send("Done"))
 
+})
+
+router.get('/checkSMS/',(req,res)=>{
+    if(req.query.otp==="7098")
+        res.send({
+            authentication:"true"
+        })
+    else
+    res.send({
+        authentication:"false"
+    })
 })
 
 module.exports = router;
--- Backend/test.js
+++
@@ -1,3 +0,0 @@
-const mongoConnection = require('./db');
-
-mongoConnection;
