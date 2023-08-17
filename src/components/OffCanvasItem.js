44202e1804628d0a7aa8c15fc116529c79fc13e0
Yash124gamer <y325dde@gmail.com>
2023-08-17 20:57:15 +0530

    Added the Route to the Settings Page to the Offcanvas Item

--- src/components/OffCanvasItem.js
+++ src/components/OffCanvasItem.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 export default function OffCanvasItem(props) {
   return (
-    <Link  to={props.destination} state={{selected:props.destination=="Login"?"Login":""}}>
-    <div className='row border border-light-subtle rounded py-2 mt-2 d-flex align-items-center'>
+    <Link  to={props.destination} style={{color:"white",textDecoration:"none"}}>
+    <div className='row border border-light-subtle rounded py-2 mt-2 d-flex align-items-center listItems'>
         <img src={props.logo} style={{width:"16%"}} />
         {props.name}
     </div>
